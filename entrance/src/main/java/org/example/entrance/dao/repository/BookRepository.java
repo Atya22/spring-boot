730261138026559fbc7e953b8cf24074f9b00835
Aytaj <141915976+Atya22@users.noreply.github.com>
2025-02-10 15:59:45 +0400

    Delete entrance directory

--- entrance/src/main/java/org/example/entrance/dao/repository/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.entrance.dao.repository;
-
-import org.example.entrance.dao.entity.BookEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<BookEntity, Long> {
-}
--- entrance/src/main/java/org/example/entrance/dao/repository/GenreRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.entrance.dao.repository;
-
-import org.example.entrance.dao.entity.GenreEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GenreRepository extends JpaRepository<GenreEntity, Long> {
-}
